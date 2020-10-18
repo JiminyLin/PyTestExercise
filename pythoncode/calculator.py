7e6c005ed740112d23c8101d53f1f7d4f10306be
Lingz <lingz@jiguang.cn>
2020-10-18 11:35:12 +0800

    优化计算器代码，补全计算器自动化测试用例，生成allure测试报告

--- pythoncode/calculator.py
+++ pythoncode/calculator.py
@@ -1,8 +1,20 @@
 class Calculator:
-    def add(self,a,b ):
-        return  a + b
 
-    def sub(self,a,b):
-        return a - b
+    def add(self, a, b):
+        return a + b
 
-    # def
+    def sub(self, a, b):
+        return round((a - b),10)
+
+    def mul(self, a, b):
+        return round((a * b),5)
+
+    def div(self, a, b):
+        try:
+            round((a / b), 5)
+            return round((a / b), 5)
+        except ZeroDivisionError as err:
+            print (f'err:{err}')
+            return "divisor can't 0"
+
+
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,27 +1,37 @@
+import allure
 import pytest
+import yaml
 
 from pythoncode.calculator import Calculator
 
 
 class TestCalc:
     def setup_class(self):
-        print('计算开始')
         self.calc = Calculator()
 
     def teardown_class(self):
-        print('计算结束')
+        pass
 
-    @pytest.mark.parametrize('a,b,expect', [[1, 2, 3], [2, 3, 4], [3.0, 6, 9],[-1,-1,-2]],ids=['int_case1','int_case2','flat_case','minus_case'])
-    def test_add(self, a, b, expect):
+    @allure.story('计算器的加法')
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./data/add.yml')))
+    def test_Add(self, a, b, expect):
         result = self.calc.add(a, b)
         assert result == expect
 
-    def test_add2(self):
-        result = self.calc.add(12, 1)
-        assert result == 2
+    @allure.story('计算器的减法')
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./data/sub.yml')))
+    def test_sub(self, a, b, expect):
+        result = self.calc.sub(a, b)
+        assert result == expect
 
-    @pytest.mark.parametrize('a,b,expect',[(2,2,0),(3,2,1),(-2,-4,2),(0,1,-1)])
-    def test_sub(self,a,b,expect):
-        result = self.calc.sub(a,b)
+    @allure.story('计算器的乘法')
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./data/mult.yml')))
+    def test_mult(self, a, b, expect):
+        result = self.calc.mul(a, b)
+        assert result == expect
 
+    @allure.story('计算器的除法')
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./data/div.yml')))
+    def test_div(self, a, b, expect):
+        result = self.calc.div(a, b)
         assert result == expect
